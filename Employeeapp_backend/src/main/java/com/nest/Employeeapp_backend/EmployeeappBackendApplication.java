4049422958dd06c1cac1bbdf61cd8c9ac62cf6e9
anugraha4 <96745531+anugraha4@users.noreply.github.com>
2022-12-05 15:35:32 +0530

    employeebackend

---
+++ Employeeapp_backend/src/main/java/com/nest/Employeeapp_backend/EmployeeappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nest.Employeeapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeappBackendApplication.class, args);
+	}
+
+}
---
+++ Employeeapp_backend/src/main/java/com/nest/Employeeapp_backend/controller/EmployeeController.java
@@ -0,0 +1,11 @@
+package com.nest.Employeeapp_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+    @GetMapping("/")
+    public
+}
