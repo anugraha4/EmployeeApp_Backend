9a5d541fbbe9729d32d39ad0179b46758506388a
anugraha4 <96745531+anugraha4@users.noreply.github.com>
2022-12-11 06:35:02 +0530

    done

--- Employeeapp_backend/src/main/java/com/nest/Employeeapp_backend/dao/EmployeeDao.java
+++ Employeeapp_backend/src/main/java/com/nest/Employeeapp_backend/dao/EmployeeDao.java
@@ -1,6 +1,8 @@
 package com.nest.Employeeapp_backend.dao;
 
 import com.nest.Employeeapp_backend.model.Employees;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -9,6 +11,12 @@ import java.util.List;
 
 public interface EmployeeDao extends CrudRepository<Employees,Integer> {
 
-    @Query(value = "SELECT `id`, `cmpname`, `designation`, `empcd`, `empname`, `mobno`, `password`, `salary`, `username` FROM `employees` WHERE `empcd`=:empcode%",nativeQuery = true)
+    @Query(value = "SELECT `id`, `cmpname`, `designation`, `empcd`, `empname`, `mobno`, `password`, `salary`, `username` FROM `employees` WHERE `empcd`=:empcode",nativeQuery = true)
     List<Employees> SearchEmployee(@Param("empcode") Integer empcode);
+
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM `employees` WHERE `id`=:id",nativeQuery = true)
+    void deleteEmployee(@Param("id")Integer id);
 }
